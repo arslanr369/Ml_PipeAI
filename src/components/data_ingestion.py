87dc277d3d9415243e39f37a9893ef0fc8e3b4a6
Arslanr369 <Arslanriaz785@gmail.com>
2024-10-20 11:22:26 +0500

    Update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -16,23 +16,19 @@ class DataIngestion:
         try:
             logging.info("Reading data using pandas from local system")
 
-            # Adjusted path to point to the correct location of 'notebook/data/income_clean.csv'
             data_path = os.path.join(os.getcwd(), "notebook", "data", "income_cleandata.csv")
             
-            # Check if the file exists
             if not os.path.exists(data_path):
                 logging.error(f"File not found: {data_path}")
                 raise FileNotFoundError(f"File not found: {data_path}")
             
-            data = pd.read_csv(data_path)  # Reading the CSV file
+            data = pd.read_csv(data_path)  
             logging.info("Data reading completed")
 
-            # Ensure the output directory exists
             os.makedirs(os.path.dirname(self.raw_data_path), exist_ok=True)
             data.to_csv(self.raw_data_path, index=False)
             logging.info("Data saved to raw data path")
 
-            # Split the data into train and test sets
             train_set, test_set = train_test_split(data, test_size=0.3, random_state=42)
 
             train_set.to_csv(self.train_data_path, index=False, header=True)
